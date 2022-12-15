2de9f0a85cdf1a3950d1b8126baa6e366ff96812
Sif Høg <cimmik@yahoo.dk>
2022-12-15 18:59:51 +0100

    Now, the page load a test preview jusing JS
    The next thing I should work on is to add non-test previews.

--- monstromusi/script.js
+++ monstromusi/script.js
@@ -1,9 +1,26 @@
-fetch("posts.json")
+fetch("https://raw.githubusercontent.com/sifhg/sifhg.github.io/main/monstromusi/posts.json")
     .then((response) => response.json())
     .then((data) => {
         const metaData = data;
         console.log(metaData);
+        document.getElementById("post-load").innerHTML = "";
+        addPreviews(0, 10);
     })
     .catch((error) => {
         console.log("ERROR: " + error);
-    }) ;
+    });
+function createP(someInner, aClass, anId) {
+    const startTag = '<p class="' + aClass + '" id="' + anId + '">';
+    const inner = someInner;
+    const endTag = '</p>';
+    return startTag + inner + endTag;
+}
+function createTr(aDate, aTitle, someContent) {
+    const startTag = "<tr><td>";
+    const inner =  "" + aDate + '</td><td><a href="?post=' + aDate + '"><h2>' + aTitle + "</h2></a><br>" + someContent;
+    const endTag = "[...]</td></tr>";
+    return startTag + inner + endTag;
+}
+function addPreviews(firstPreview, lastPreview) {
+    document.getElementById("post-list").innerHTML += createTr("2022-12-15", "TodayToday", "Her er der rigtig mange");
+}
