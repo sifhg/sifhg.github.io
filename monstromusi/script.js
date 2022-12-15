8a1e7c7a2a212ebba57a3a74f20764d89f60377a
Sif Høg <cimmik@yahoo.dk>
2022-12-16 00:33:32 +0100

    fine CSS adjustments

--- monstromusi/script.js
+++ monstromusi/script.js
@@ -1,10 +1,21 @@
+const queryString = window.location.search;
+const urlParameters = new URLSearchParams(queryString);
+let postContent;
+
 fetch("https://raw.githubusercontent.com/sifhg/sifhg.github.io/main/monstromusi/posts.json")
     .then((response) => response.json())
     .then((data) => {
         const metaData = data;
         console.log(metaData);
         document.getElementById("post-load").innerHTML = "";
-        addPreviews(0, 10);
+        
+        if(urlParameters.get('post') == null) {
+            addPreviews(0, 10);
+        }else {
+            document.getElementById("post-list").remove();
+            document.getElementById("more-posts").remove();
+            loadPost(urlParameters.get('post'));
+        }
     })
     .catch((error) => {
         console.log("ERROR: " + error);
@@ -23,4 +34,20 @@ function createTr(aDate, aTitle, someContent) {
 }
 function addPreviews(firstPreview, lastPreview) {
     document.getElementById("post-list").innerHTML += createTr("2022-12-15", "TodayToday", "Her er der rigtig mange");
+}
+function loadPost(postName) {
+    fetch("https://sifhg.github.io/monstromusi/" + postName + ".html")
+    .then((response) => {
+        if(!response.ok) {
+            window.location.href = "https://sifhg.github.io/monstromusi/";
+        }
+        return response.text();
+    })
+    .then((html) => {
+        postContent = html;
+        document.getElementsByTagName("body")[0].innerHTML += postContent;
+    })
+    .catch((error) => {
+        console.log("ERROR: "+ error);
+    })
 }
