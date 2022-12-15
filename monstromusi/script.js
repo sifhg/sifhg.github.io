3febfd74e13b65961cbf565fa1a3de3314d1e1dc
Sif Høg <cimmik@yahoo.dk>
2022-12-15 18:30:11 +0100

    js added

--- monstromusi/script.js
+++ monstromusi/script.js
@@ -1,1 +1,9 @@
-//const meta = fetch();
+fetch("posts.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const metaData = data;
+        console.log(metaData);
+    })
+    .catch((error) => {
+        console.log("ERROR: " + error);
+    }) ;
