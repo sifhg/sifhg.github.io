8543716b2aacdeb27517606a76e9db76b5e649ac
Sif Høg <cimmik@yahoo.dk>
2023-04-22 14:57:03 +0200

    Worked with top menu
    Next, you want to use :after or to add underline under each menu part. Should be interactive so they go from 0-50%, or maybe 10-50% when hovering over them.
    After, you should find a way to change the name of the last part depending on meta head title.

--- sifcity/headerAdjuster.js
+++ sifcity/headerAdjuster.js
@@ -1,11 +1,11 @@
 function resizeHeader() {
     const HEADER = document.querySelector("embed");
     if(window.innerWidth < 895) {
-        let newHeight = window.innerWidth*(49/150) + (48);
+        let newHeight = window.innerWidth*(49/150) + (68);
         newHeight = (newHeight+"").split(".")[0] + "px";
         HEADER.style.height = newHeight;
     }else {
-        HEADER.style.height = "340px";
+        HEADER.style.height = "360px";
     }
 }
 addEventListener("DOMContentLoaded", (event) => {
