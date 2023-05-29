cb00f3db55f258afa712fb574bbb44185a3fe597
Sif Høg <cimmik@yahoo.dk>
2023-05-29 14:01:28 +0200

    polarizor() function defined

--- sifcity/p5/sketch.js
+++ sifcity/p5/sketch.js
@@ -37,6 +37,29 @@ class City {
         this.buildinigs.splice(index, 0);
     }
 }
+
+function polarizor(x, f) {
+    //Link to Mathematical formula: https://www.geogebra.org/classic/qbcf5yea
+    //x is the input value to be polarized.
+    //f is the force of which x will be polarized.
+    //if f is 0, x will be polarized to either 0 or 1.
+    //if f is 1, x almost wont be changed.
+
+    if(typeof x !== 'number' || typeof f !== 'number') {
+        throw new Error(`Inputs must be numbers. \nType of x is "${typeof x}"\nType of f is "${typeof f}"`);
+    }
+    if(x > 1 || x < 0) {
+        console.warn(`WARNING: polarizor() might not work as intended when the input, x is out of range (0 to 1)\nx = ${x}`);
+    }
+    if(x > .5) {
+        return ((x*f) + (1*(x-.5))) / (f + (x-.5));
+    } else if(x < .5) {
+        return ((x*f) + (0*(-2*x +1))) / (f + (1-(2*x)));
+    } else {
+        return .5;
+    }
+}
+
 function setup() {
     let headerAnimation = document.getElementById("header-animation");
     let ani = createCanvas(headerAnimation.offsetWidth, headerAnimation.offsetHeight);
