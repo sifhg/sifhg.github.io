cd9535cba7cbc4830b7826101c0e5731203ee429
Sif Høg <cimmik@yahoo.dk>
2023-07-07 01:04:01 +0200

    Lots of styling

--- fallout_quiz/script.js
+++ fallout_quiz/script.js
@@ -1,4 +1,17 @@
 let questions;
+let shuffleCode;
+const OPENING_WINDOW = document.getElementById("opening-window");
+const GAME_WINDOW = document.getElementById("game-window");
+const INPUT_BOX = document.querySelector("input[type='text']");
+
+  
+INPUT_BOX.addEventListener("focus", (event) => {
+    console.log("Active");
+});
+  
+INPUT_BOX.addEventListener("blur", (event) => {
+    console.log("Inactive");
+})
 
 fetch("https://raw.githubusercontent.com/sifhg/sifhg.github.io/main/fallout_quiz/questions_and_answers.json")
     .then((response) => response.json())
@@ -6,3 +19,22 @@ fetch("https://raw.githubusercontent.com/sifhg/sifhg.github.io/main/fallout_quiz
         questions = data;
         console.log(questions);
     });
+
+function getShuffleCode() {
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    if(!urlParams.has("SC")) {
+        return false;
+    }
+    return urlParams.get("SC");
+}
+
+shuffleCode = getShuffleCode();
+if(shuffleCode === false) {
+    OPENING_WINDOW.style.display = "block";
+    GAME_WINDOW.style.display = "none";
+}else {
+    OPENING_WINDOW.style.display = "none";
+    GAME_WINDOW.style.display = "block";
+}
+
