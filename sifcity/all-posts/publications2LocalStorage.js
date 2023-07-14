81e66605f5a9c3537aa3abeba5b99b7e0a32fbe1
Sif Høg <cimmik@yahoo.dk>
2023-07-14 14:26:49 +0200

    Added a bit more for the places page
    Next, you should make a test page for Superkilen under "sifcity/places/superkilen" and the "places" page. under "sifcity/places"

--- sifcity/all-posts/publications2LocalStorage.js
+++ sifcity/all-posts/publications2LocalStorage.js
@@ -1,3 +1,5 @@
+//Blog posts
+
 const TABLE_ROWS = document.getElementsByClassName("publication");
 let publications = [];
 for(const PUBLICATION of TABLE_ROWS) {
@@ -13,3 +15,21 @@ for(const PUBLICATION of TABLE_ROWS) {
 }
 publications.push(new Date());
 localStorage.setItem("allPublications", JSON.stringify(publications));
+
+
+//Place pages
+const PLACE_ROWS = document.getElementsByClassName("place");
+let placePages =[];
+for(const PAGE of PLACE_ROWS) {
+    const LINK_ELEMENT = PAGE.querySelector(".place-title a");
+    const TO_STORAGE = {
+        title: LINK_ELEMENT.innerText,
+        link: LINK_ELEMENT.getAttribute("href"),
+        author: PAGE.querySelector(".place-author").innerText,
+        description: PAGE.querySelector(".place-description").innerText
+    }
+    placePages.push(TO_STORAGE)
+}
+placePages.push(new Date());
+localStorage.setItem("allPlaces", JSON.stringify(placePages));
+
