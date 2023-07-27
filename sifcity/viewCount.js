6ac78440908608cfa9246b5679192f0e5841e63f
Sif Høg <cimmik@yahoo.dk>
2023-07-27 16:07:22 +0200

    Test visit counter

--- sifcity/viewCount.js
+++ sifcity/viewCount.js
@@ -2,66 +2,78 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebas
 import { getAuth, onAuthStateChanged } from  "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import  { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-const CONFIG = {
-    apiKey: "AIzaSyDnrO6FHN4tb5unnbNEAhXv9UfwsZLhsgo",
-    authDomain: "sif-city-view-counter.firebaseapp.com",
-    projectId: "sif-city-view-counter",
-    storageBucket: "sif-city-view-counter.appspot.com",
-    messagingSenderId: "627953674704",
-    appId: "1:627953674704:web:a3c38c121cb7895c77f1d5"
-  };
+if(window.self == window.top) {
 
-// Initialize Firebase
-const APP = initializeApp(CONFIG);
+    // Your web app's Firebase configuration
+    const CONFIG = {
+        apiKey: "AIzaSyDnrO6FHN4tb5unnbNEAhXv9UfwsZLhsgo",
+        authDomain: "sif-city-view-counter.firebaseapp.com",
+        projectId: "sif-city-view-counter",
+        storageBucket: "sif-city-view-counter.appspot.com",
+        messagingSenderId: "627953674704",
+        appId: "1:627953674704:web:a3c38c121cb7895c77f1d5"
+    };
 
-//Obtain authentication instance app, to check if the app works
-const AUTH = getAuth(APP);
+    // Initialize Firebase
+    const APP = initializeApp(CONFIG);
 
-onAuthStateChanged(AUTH, user => {
-    if(user != null) {
-        console.log("logged in!");
-    }else {
-        console.log("No user.")
-    }
-});
+    //Obtain authentication instance app, to check if the app works
+    const AUTH = getAuth(APP);
 
-// Initialize Cloud Firestore and get a reference to the service
-const DB = getFirestore(APP);
-
-//Write data
-/* try {
-    const DOC_REF = await addDoc(collection(DB, "users"), {
-        first: "Alan",
-        middle: "Mathison",
-        last: "Turing",
-        born: 1912
+    onAuthStateChanged(AUTH, user => {
+        if(user != null) {
+            console.log("logged in!");
+        }else {
+            console.log("No user.")
+        }
     });
-    console.log("Document written with ID: ", DOC_REF.id);
-}catch(e) {
-    console.error("ERROR adding document: ", e);
-} */
 
-//Read data
-const SNAPSHOT = await getDocs(collection(DB, "users"));
-SNAPSHOT.forEach((doc) => {
-    console.log(doc.id);
-    console.log(doc.data());
-})
+    // Initialize Cloud Firestore and get a reference to the service
+    const DB = getFirestore(APP);
 
-sessionStorage.setItem("title", document.querySelector("title").innerText);
+    //Write data
+    /* try {
+        const DOC_REF = await addDoc(collection(DB, "users"), {
+            first: "Alan",
+            middle: "Mathison",
+            last: "Turing",
+            born: 1912
+        });
+        console.log("Document written with ID: ", DOC_REF.id);
+    }catch(e) {
+        console.error("ERROR adding document: ", e);
+    } */
 
-if(!sessionStorage.getItem("session-start")) {
-    sessionStorage.setItem("session-start", new Date());
+    //Read data
+    const SNAPSHOT = await getDocs(collection(DB, "users"));
+    SNAPSHOT.forEach((doc) => {
+        console.log(doc.id);
+        console.log(doc.data());
+    })
+
+    sessionStorage.setItem("title", document.querySelector("title").innerText);
+
+    if(!sessionStorage.getItem("session-start")) {
+        sessionStorage.setItem("session-start", Date.parse(new Date()));
+    }
+
+    const SESSION_DATA = {
+        title: document.querySelector("title").innerText,
+        webLocation: document.URL,
+        time: Date.parse(new Date()),
+        previous: document.referrer,
+        sessionStart: sessionStorage.getItem("session-start"),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    }
+
+    console.log(SESSION_DATA.sessionStart == SESSION_DATA.time);
+    console.log(SESSION_DATA);
+    if(SESSION_DATA.sessionStart == SESSION_DATA.time) {
+        try {
+            const DOC_REF = await addDoc(collection(DB, "visits"), SESSION_DATA);
+            console.log("Session logged with ID: ", DOC_REF.id);
+        }catch(e) {
+            console.error("ERROR adding document: ", e);
+        }
+    }
 }
-
-const SESSION_DATA = {
-    title: document.querySelector("title").innerText,
-    webLocation: document.URL,
-    time: new Date(),
-    previous: document.referrer,
-    sessionStart: sessionStorage.getItem("session-start"),
-    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
-}
-
-console.log(SESSION_DATA);
