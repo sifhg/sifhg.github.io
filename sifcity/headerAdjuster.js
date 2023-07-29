357cc073735d70bc778bf1ac58ce960567e77d3d
Sif Høg <cimmik@yahoo.dk>
2023-07-29 14:55:15 +0200

    Updated Sif City front page

--- sifcity/headerAdjuster.js
+++ sifcity/headerAdjuster.js
@@ -1,5 +1,6 @@
 function resizeHeader() {
     const HEADER = document.querySelector("embed");
+
     if(window.innerWidth < 895) {
         let newHeight = window.innerWidth*(49/150) + (68);
         newHeight = (newHeight+"").split(".")[0] + "px";
@@ -7,6 +8,9 @@ function resizeHeader() {
     }else {
         HEADER.style.height = "360px";
     }
+    console.log(`Window width: ${window.innerWidth}`);
+    console.log(`Width: ${HEADER.style.width}`);
+    console.log(`Height: ${HEADER.style.height}`);
 }
 addEventListener("DOMContentLoaded", (event) => {
     resizeHeader();
